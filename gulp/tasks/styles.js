48604362223c5e9164143b5e929683e5fcd93082
Hsins <hsinspeng@gmail.com>
2019-03-08 16:37:13 +0800

    Completed automatic sprite generation.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -10,7 +10,7 @@ gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .on('error', function(errorInfo) {
-      console.log(errorInfo.toString);
+      console.log(errorInfo.toString());
       this.emit('end');
     })
     .pipe(gulp.dest('./app/temp/styles'));
